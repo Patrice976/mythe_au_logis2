a14348644bd58928bad2934c22dc8421f476398f
Patrice976 <catiatp@gmail.com>
2024-07-03 15:51:25 +0200

    function généralisé display button ok display

--- index.js
+++ index.js
@@ -20,25 +20,53 @@ window.addEventListener('scroll', function() {
 
 const ButtonToDisplay = {
     'buttongreek' : 'display_greek',
-    'buttonnorse' : 'display_norse'
-
+    'buttonnorse' : 'display_norse',
+    'buttonegyptian' : 'display_egyptian',
+    'buttonhindu' : 'display_hindu',
+    'buttonchinois' : 'display_chinois',
+    'buttonjapan' : 'display_japan',
+    'buttonarthurian' : 'display_arthurian',
+    'buttonmayan' : 'display_mayan',
+    'buttonceltic' : 'display_celtic',
 }
-let buttongreek = document.getElementById('buttongreek')
-let visible=false
+let buttons = document.querySelectorAll('#boutonpantheon button')
+console.log(buttons)
+
+for (let index = 0; index < buttons.length; index++) {
+    const element = buttons[index];
+    element.addEventListener ('click', function() {
+        alert('coucou')
+         // ON doit recuperer l'id du bouton clické
+        let id = this.getAttribute('id')
+        // EN utilisant ton objet ButtonToDIsplay, recuperer l'id correspondant de la div
+        let display = ButtonToDisplay[id]
+        // Selectionner la div grace a son id
+        let target_div = document.getElementById(display)
+        console.log(target_div)
+        // Afficher la div
+        target_div.classList.remove('hide')
+
+    })
+}
+
+// let buttongreek = document.getElementById('buttongreek')
+// let visible=false
 
 
-buttongreek.addEventListener ('click', function() {
+// buttongreek.addEventListener ('click', function() {
 
-    const div = document.getElementById('hidden')
+//     const div = document.getElementById('display_greek')
 
-    if (visible==false) {
-        div.classList.remove('hide') ;
-        visible=true
+//     if (visible==false) {
+//         div.classList.remove('hide') ;
+//         visible=true
  
-    } else { 
-    div.classList.add('hide')
-    visible = false
-    }
+//     } else { 
+//     div.classList.add('hide')
+//     visible = false
+//     }
    
-})
+// })
+
+
 
