49354a47ff8d234428de116a37a7aa892b284722
Patrice976 <catiatp@gmail.com>
2024-07-16 10:32:06 +0200

    Code cleaned : rename Id variable class

--- index.js
+++ index.js
@@ -2,52 +2,52 @@
 
 window.addEventListener('scroll', function() {
 
-    const header = document.querySelector('header');
-    let logo = document.getElementById('logo') ;
+    const Header = document.querySelector('header');
+    let LogoLandingPage = document.getElementById('logo') ;
     let Y = scrollY ;
 
        if (window.scrollY > 0) {
-        header.classList.add('Reduce');
-        logo.src= 'ressourceimg/logo image - blanc.png'
+        Header.classList.add('Reduce');
+        LogoLandingPage.src= 'ressourceimg/logo image - blanc.png'
 
     } else {
-        header.classList.remove('Reduce');
-        logo.src= 'ressourceimg/Logo ligne - blanc.png'
+        Header.classList.remove('Reduce');
+        LogoLandingPage.src= 'ressourceimg/Logo ligne - blanc.png'
     }
 })
 
 // Bonton panthéon affichage de contenu au clic  //
 const ButtonToDisplay = {
-    'buttongreek' : 'display_greek',
-    'buttonnorse' : 'display_norse',
-    'buttonegyptian' : 'display_egyptian',
-    'buttonhindu' : 'display_hindu',
-    'buttonchinois' : 'display_chinois',
-    'buttonjapan' : 'display_japan',
-    'buttonarthurian' : 'display_arthurian',
-    'buttonmayan' : 'display_mayan',
-    'buttonceltic' : 'display_celtic',
+    'button_greek' : 'display_greek',
+    'button_norse' : 'display_norse',
+    'button_egyptian' : 'display_egyptian',
+    'button_hindu' : 'display_hindu',
+    'button_chinois' : 'display_chinois',
+    'button_japan' : 'display_japan',
+    'button_arthurian' : 'display_arthurian',
+    'button_mayan' : 'display_mayan',
+    'button_celtic' : 'display_celtic',
 }
-let buttons = document.querySelectorAll('#boutonpantheon button')
-let display_pantheon = document.querySelectorAll('.display_pantheon section')
+let BoutonPantheon = document.querySelectorAll('#bouton_pantheon button')
+let DisplayPantheon = document.querySelectorAll('.display_pantheon section')
 console.log(buttons)
 
-for (let index = 0; index < buttons.length; index++) { //On va parcourir l'enssemble des boutons 
-    const element = buttons[index];
+for (let index = 0; index < BoutonPantheon.length; index++) { //On va parcourir l'enssemble des boutons 
+    const element = BoutonPantheon[index];
     element.addEventListener ('click', function() { //On va attaché au boutton cliqué un eventListener 
 
-            for (let i = 0; i < display_pantheon.length; i++) {  //boucle pour cacher toute les éléments display
-                const screen = display_pantheon[i]; //On parcours l'enssemble des éléments du display panthéon 
-                screen.classList.add('hide') // On va cacher ces éléments 
+            for (let i = 0; i < DisplayPantheon.length; i++) {  //boucle pour cacher toute les éléments display
+                const ButtonContent = DisplayPantheon[i]; //On parcours l'enssemble des éléments du display panthéon 
+                ButtonContent.classList.add('hide') // On va cacher ces éléments 
             }
          // ON doit recuperer l'id du bouton clické
-        let id = this.getAttribute('id')
+        let ButtonID = this.getAttribute('id')
         // EN utilisant ton objet ButtonToDIsplay, recuperer l'id correspondant de la div
-        let display = ButtonToDisplay[id]
+        let Display = ButtonToDisplay[ButtonID]
         // Selectionner la div grace a son id
-        let target_div = document.getElementById(display)
+        let TargetDiv = document.getElementById(Display)
         // Afficher la div
-        target_div.classList.remove('hide')
+        TargetDiv.classList.remove('hide')
     })
 }
 
